41c97899e2162bfbc5c0a5cd8029677e976d9746
Stefan Iordache <stefan.iordache2001@gmail.com>
2025-03-10 21:07:44 +0200

    initial server configuration
    connect the server to the MQTT broker over TLS and test subscribing to
    an event
    
    Signed-off-by: Stefan Iordache <stefan.iordache2001@gmail.com>

---
+++ server/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+func NewTLSConfig() *tls.Config {
+	// Import trusted certificates from CAfile.pem.
+	// Alternatively, manually add CA certificates to
+	// default openssl CA bundle.
+	certpool := x509.NewCertPool()
+	pemCerts, err := os.ReadFile("../certs/ca.crt")
+	if err != nil {
+		panic(err)
+	}
+	certpool.AppendCertsFromPEM(pemCerts)
+
+	// Import client certificate/key pair
+	cert, err := tls.LoadX509KeyPair("../certs/web.crt", "../certs/web.key")
+	if err != nil {
+		panic(err)
+	}
+
+	// Create tls.Config with desired tls properties
+	return &tls.Config{
+		// RootCAs = certs used to verify server cert.
+		RootCAs: certpool,
+		// ClientAuth = whether to request cert from server.
+		// Since the server is set up for SSL, this happens
+		// anyways.
+		ClientAuth: tls.NoClientCert,
+		// ClientCAs = certs used to validate client cert.
+		ClientCAs: certpool,
+		// InsecureSkipVerify = verify that cert contents
+		// match server. IP matches what is in cert etc.
+		InsecureSkipVerify: true,
+		// Certificates = list of certs client sends to server.
+		Certificates: []tls.Certificate{cert},
+	}
+}
+
+var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("TOPIC: %s\n", msg.Topic())
+	fmt.Printf("MSG: %s\n", msg.Payload())
+}
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	c := make(chan os.Signal, 1)
+
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	tlsconfig := NewTLSConfig()
+
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker("ssl://broker:8883")
+	opts.SetClientID("web").SetTLSConfig(tlsconfig)
+	opts.SetDefaultPublishHandler(f)
+
+	// Start the connection
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	// Subscribe to a Topic
+	if token := client.Subscribe("general", 0, f); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
+
+	<-c
+}
